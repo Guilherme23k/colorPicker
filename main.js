601c62ded854194735bda59dc8f81875ca87a3bc
Guilherme Ramos <guilhermeggr23@gmail.com>
2023-01-08 22:04:12 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const btn = document.getElementById('btn');
+
+const color = document.getElementById('color');
+
+const reset = document.getElementById('reset');
+
+//Lista de hexColor
+const colorList = [0,1,2,3,4,5,6,7,8,9, "A","B","C","D","E","F"]
+
+btn.addEventListener('click', () =>{
+
+    let hexColor = '#';
+
+    for(let i = 0; i<6; i++){
+        hexColor += colorList[genRandomNumber()];
+    }
+
+    color.textContent = hexColor;
+
+    document.body.style.backgroundColor = hexColor
+
+
+
+    
+})
+
+
+
+reset.addEventListener('click', () =>{
+    color.textContent = '#FFFFFF';
+    document.body.style.backgroundColor = '#FFFFFF'
+})
+
+
+
+function genRandomNumber(){
+    //Gera um número aleatório baseado no tamanho do array colorList
+    return Math.floor(Math.random() * colorList.length);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily:{
+      Roboto : "Roboto"
+      },
+      letterSpacing:{
+        wideButton : '0.25rem'
+      },
+      colors: {
+        blackMain : '#222',
+        blueMain : '#49a6e9',
+        greenAlert : '#c8f2db',
+        greenAlertBorder : '#40bc70'
+      }
+    },
+  },
+  plugins: [],
+}
